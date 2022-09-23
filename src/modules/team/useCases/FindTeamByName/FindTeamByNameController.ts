630146326f6b1099fc2d4df95b264980daebb9df
Angelo Simoura <C0642474@vale.com>
2022-09-23 17:40:56 -0300

    update Swagger, FindByNameUseCase and Controller

--- src/modules/team/useCases/FindTeamByName/FindTeamByNameController.ts
+++ src/modules/team/useCases/FindTeamByName/FindTeamByNameController.ts
@@ -5,7 +5,7 @@ import { FindTeamByNameUseCase } from './FindTeamByNameUseCase';
 
 export class FindTeamByNameController {
   public async handle(request: Request, response: Response): Promise<Response> {
-    const teamName  = request.query;
+    const {teamName}  = request.query;
     const findTeamByNameUseCase = container.resolve(FindTeamByNameUseCase);
 
     const teamResponse = await findTeamByNameUseCase.execute({
--- src/modules/team/useCases/FindTeamByName/FindTeamByNameUseCase.ts
+++ src/modules/team/useCases/FindTeamByName/FindTeamByNameUseCase.ts
@@ -4,13 +4,19 @@ import { AppError } from '@shared/errors/AppError';
 
 import { ITeamsRepository } from '@modules/team/repositories/ITeamsRepository';
 
+
+export interface IFindTeamByNameDTO {
+  teamName: string;
+}
+
 @injectable()
 export class FindTeamByNameUseCase {
   constructor(
     @inject('TeamsRepository')
     private teamsRepository: ITeamsRepository,
   ) {}
-  public async execute({teamName}): Promise<Team> {
+
+  public async execute({teamName}: IFindTeamByNameDTO): Promise<Team> {
 
     const teamExists = await this.teamsRepository.findByName(teamName);
     if (!teamExists) throw new AppError('Team not found');
