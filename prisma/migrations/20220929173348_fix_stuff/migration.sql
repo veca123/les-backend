00b4e9c5b57befd7e5ba492dc26faf2f665c362a
guilherme <joao.g.f@icloud.com>
2022-09-29 14:34:09 -0300

    remove migration

--- prisma/migrations/20220929173348_fix_stuff/migration.sql
+++ prisma/migrations/20220929173348_fix_stuff/migration.sql
@@ -2,6 +2,7 @@
   Warnings:
 
   - You are about to drop the column `locationId` on the `Event` table. All the data in the column will be lost.
+  - You are about to drop the `Location` table. If the table is not empty, all the data it contains will be lost.
 
 */
 -- DropForeignKey
@@ -10,3 +11,6 @@ ALTER TABLE "Event" DROP CONSTRAINT "Event_locationId_fkey";
 -- AlterTable
 ALTER TABLE "Event" DROP COLUMN "locationId",
 ADD COLUMN     "location" TEXT;
+
+-- DropTable
+DROP TABLE "Location";
