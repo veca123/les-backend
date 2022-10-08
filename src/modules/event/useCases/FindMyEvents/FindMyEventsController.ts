e1cf5318b17e400c4f9e3e60c2f0df6fe5691808
joaogf <joao.g.f@icloud.com>
2022-10-08 13:04:47 -0300

    fix

--- src/modules/event/useCases/FindMyEvents/FindMyEventsController.ts
+++ src/modules/event/useCases/FindMyEvents/FindMyEventsController.ts
@@ -5,7 +5,7 @@ import { FindMyEventsUseCase } from './FindMyEventsUseCase';
 
 export class FindMyEventsController {
   public async handle(request: Request, response: Response): Promise<Response> {
-    const { id } = request.user;
+    const { id } = request.params;
 
     const findMyEventsUseCase = container.resolve(FindMyEventsUseCase);
 
